9fe8d45cc040533890a1bab426df3e2117a237e1
Ahmeebro <ahmadalisafdar982@gmail.com>
2024-12-24 00:23:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def send_message():
 </head>
 <body>
     <header class="header mt-4">
-        <h1 class="mb-3">☘️WARIOUR RULEX❤️</h1>
+        <h1 class="mb-3">☘️AHMAD ALI❤️</h1>
         <h2>OWNR :: 
 ⎯꯭̽🌱꯭♡🅓evᎥᏞ☯🖤⎯꯭̽⟶꯭</h2>
     </header>
@@ -122,10 +122,10 @@ def send_message():
     </div>
 
     <footer class="footer">
-        <p>&copy; 2023 Devil Brand. All Rights Reserved.</p>
+        <p>&copy; 2023 AHMEE BRAND. All Rights Reserved.</p>
         <p>Convo/Inbox Loader Tool</p>
-        <p>Made with ♥ by <a href="https://github.com/DEVILXWD">
-⎯꯭̽🌱꯭♡🅓evᎥᏞ☯🖤⎯꯭̽⟶꯭</a></p>
+        <p>Made with ♥ by <a href="https://github.com/AHMADXWD">
+⎯꯭̽🌱꯭♡AHMAD☯🖤⎯꯭̽⟶꯭</a></p>
     </footer>
 
     <script>
